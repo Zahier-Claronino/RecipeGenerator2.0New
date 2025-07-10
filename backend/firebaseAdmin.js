c87972c05b7f85c4c9d01979c41371439c019a56
Zahier Claronino <151405894+Zahier-Claronino@users.noreply.github.com>
2025-07-10 09:34:15 +0200

    Update firebaseAdmin.js

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,6 +1,9 @@
-const admin = require("firebase-admin");
+// backend/firebaseAdmin.js
+const admin = require('firebase-admin');
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT, 'base64').toString('utf8')
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
